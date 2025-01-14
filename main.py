0424f39c71f053a824b18a6bd83a2d49a9cac517
bewareofducks <shumack908@gmail.com>
2025-01-14 02:44:12 -0500

    D: should be done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def word_count(text):
     words = text.split() # splits the text into words
-    length = len(words) # length = how many words are in text
-    return(f"{length} words were found in the text") # returns the function to be used later
+    return len(words) 
+    
 
 
 
@@ -15,23 +15,38 @@ def char_count(text):
             
     count = {}  # empty dictionary to hold counts
 
-    char.list = []
-    for char, num in count.items():
-        
-    
     for char in filtered_text: #loop through each character in text
         if char in count: # if character is already in dictionary
             count[char] += 1 # increment by 1
         else:
             count[char] = 1 # else, add it with a count of 1
     
-    return count # returns the function to be used later
+    # converting to list of dictionaries
+    char_list = []  # new list to hold dictionaries
+    for char, num in count.items():
+        char_dict = {"char": char, "num": num}  # create dictionary for this character
+        char_list.append(char_dict)  # add it to the list
+
+    def sort_on(dict):
+        return dict["num"]
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
 
 def main():
     with open("books/frankenstein.txt") as f: # opens and reads frankenstein.txt
         file_contents = f.read()
-    print(word_count(file_contents)) 
-    print(char_count(file_contents)) 
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count(file_contents)} words found in the document\n") 
+
+
+    char_list = char_count(file_contents)
+    for char_dict in char_list:
+          print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+    
+    print("--- End report ---")
 
 main()
 
